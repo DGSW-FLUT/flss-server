e6dc7527f51d03e1a2433d6e0c7be7763d2a1cbb
tbvjaos510 <tbvjaos510@naver.com>
2018-09-18 20:35:25 +0900

    Revert "[MODIFY] Some commit bug"
    This reverts commit 34eecbbe0d8ff9091b8d49bf6090aaeb011e564b.

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,10 @@
 $router->get('/', 'TestController');
 $router->post('/', 'TestController');
 
-$router->group(['prefix' => 'api'], function () use ($router) {
-    $router->group(['prefix' => 'auth'], function () use ($router) {
-        $router->get('get_token', function () use ($router) {
-            return response()->make(include 'get_token.html', 200);
-        });
-        $router->get('classting', 'OAuthController@classting');
-        $router->get('classting_handle', 'OAuthController@classtingHandle');
+$router->group(['prefix' => 'auth'], function () use ($router) {
+    $router->get('get_token', function () use ($router) {
+        return response()->make(include 'get_token.html', 200);
     });
+    $router->get('classting', 'OAuthController@classting');
+    $router->get('classting_handle', 'OAuthController@classtingHandle');
 });
